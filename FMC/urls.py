8457db7a75b4b76e5498e79231bd90737a7e6636
Prakhar Bansal <prakhar.bansal.cse15@iitbhu.ac.in>
2018-10-23 17:00:21 +0530

    update code

--- FMC/urls.py
+++ FMC/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from . import views
+
 urlpatterns = [
     url(r'^clubs/$', views.clubs,name='clubs'),
     url(r'^home/$',views.home,name='home'),
